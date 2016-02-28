bf22d00f131f768dfb04507c6d4e9617e14de8a0
joshherkness <joshherkness@gmail.com>
2016-02-28 04:31:40 -0500

    Added recipe intent

--- src/index.js
+++ src/index.js
@@ -65,6 +65,9 @@ Ramsay.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, s
 
 Ramsay.prototype.intentHandlers = {
     // register custom intent handlers
+    "RecipeIntent": function (intent, session, response) {
+        recipeIntentHelper(intent, session, response);
+    },
     "IngredientIntent": function (intent, session, response) {
         listIngredients(intent, session, response);
     },
@@ -103,6 +106,16 @@ function listIngredients(intent, session, response){
           }
         }
         response.tell(s);
+    });
+}
+
+function recipeIntentHelper(intnet, session, response) {
+    var request = getRecipeRequest(intent, session, response);
+
+    getRecipes(request, function (recipes) {
+        // Pick a randome element of the recipes that are returned.
+        var randomRecipe = recipes[Math.floor(Math.random()*recipes.length)];
+        response.tell("How about " + randomRecipe.title);
     });
 }
 
